f69cbb14d76738a957f8ff3962d65d2630ebb496
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-10-23 17:58:03 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,15 +1,53 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { HomeIcon, ListOrderedIcon, LogInIcon, LogOutIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
-    <Card className="flex items-center p-[1.875rem] justify-between">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <div className="mt-2 flex flex-col items-center gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size="16" />
+              Fazer Login
+            </Button>
 
-      <h1 className="text-lg font-semibold"><span className="text-primary">FSW</span> Store</h1>
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size="16" />
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size="16" />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size="16" />
+              Catálogo
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogOutIcon size="16" />
+              Logout
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
+      </h1>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
