0238c4323a2afb9d41673055482eea9dccec4388
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-10-23 13:06:25 -0300

    feat: add button, card, and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex justify-between items-center'>
-      <h1>Home Start</h1>
-      <h1>Home Center</h1>
-      <h1>Home End</h1>
-    </div>
+    <div>Home</div>
   )
 }
