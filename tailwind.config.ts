9843d83bbf1d779f71a9d4ea44e93b62082a7eed
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-11-04 19:35:50 -0300

    fix: ensure correct gradient CategoryItem to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        "category-item-gradient": "background: linear-gradient(45deg, #5033C3 0%, rgba(80, 51, 195, 0.20) 100%)"
+        "category-item-gradient": "linear-gradient(45deg, #5033C3 0%, rgba(80, 51, 195, 0.20) 100%)"
       },
       colors: {
         border: "hsl(var(--border))",
