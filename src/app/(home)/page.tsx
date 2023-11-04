47c49c98d0d3c327ddcbf3b091b028cf25c64012
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-11-04 10:54:17 -0300

    feat: add keyboards section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Categories from "./components/categories";
 import ProductList from "./components/product-list";
 import { prismaClient } from "@/lib/prisma";
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,20 +14,37 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
   return (
     <div className="p-4">
-      <Image
+      <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto só esse mês"
-        height={0}
-        width={0}
-        sizes="100vw"
-        className="h-auto w-full"
       />
 
       <Categories />
 
-      <ProductList products={deals} />
+      <div className="mt-4">
+        <SectionTitle>OFERTAS</SectionTitle>
+        <ProductList products={deals} />
+      </div>
+
+      <PromoBanner
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto em mouses"
+      />
+
+      <div className="mt-4">
+        <SectionTitle>TECLADOS</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
     </div>
   );
 }
