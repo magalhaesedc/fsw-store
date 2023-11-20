e9e4961a7fb2d81baf11f265dcb8d58ec179af12
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-11-19 21:53:16 -0300

    feat: add createOrder server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,11 +9,20 @@ import { Button } from "./button";
 import { ScrollArea } from "./scroll-area";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useSession } from "next-auth/react";
+import { createOrder } from "@/actions/order";
 
 const Cart = () => {
+  const { data } = useSession();
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
+    if (!data?.user) {
+      return;
+    }
+
+    await createOrder(products, (data?.user as any).id);
+
     const checkout = await createCheckout(products);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
