159d86e88048a8e6e2d87e96cb2cef15a854c45b
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-11-25 00:10:19 -0300

    modify: change title, footer and header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -154,7 +154,7 @@ const Header = () => {
 
       <Link href="/">
         <h1 className="text-lg font-semibold">
-          <span className="text-primary">FSW</span> Store
+          <span className="text-primary">EDSON</span> Store
         </h1>
       </Link>
 
