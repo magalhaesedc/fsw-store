8f95636db10507da8c992cbededc0237895cdc87
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-11-24 23:04:00 -0300

    feat: add LoadingPage component

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { TailSpin } from "react-loader-spinner";
+
+const LoadingPage = () => {
+  return (
+    <div className="flex w-full flex-1 flex-col items-center p-[50%] px-20">
+      <TailSpin
+        height="80"
+        width="80"
+        color="#5033C3"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default LoadingPage;
