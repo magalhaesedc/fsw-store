25def31ed4dd4a1dc611d22629b03460efd38694
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-12-03 13:11:52 -0300

    feat: add message for empty orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -31,16 +31,26 @@ const OrderPage = async () => {
   });
 
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8  p-5">
       <Badge variant={"heading"}>
         <PackageSearchIcon size={16} /> Meus Pedidos
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {orders.map((order) => (
-          <OrderItem key={order.id} order={order} />
-        ))}
-      </div>
+      {orders.length > 0 ? (
+        <div className="flex flex-col gap-5">
+          {orders.map((order) => (
+            <OrderItem key={order.id} order={order} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-2 p-5">
+          <h2 className="font-bold">Ainda não há pedidos!</h2>
+          <p className="text-center text-sm opacity-60">
+            Assim que concluir suas compras, seus pedidos serão apresentados
+            aqui.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
