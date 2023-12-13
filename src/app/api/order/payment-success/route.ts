a07458e367c40be49a4137d5fdece18c5b3d4ade
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-12-13 13:19:39 -0300

    fix: error webhook

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -8,11 +8,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export const POST = async (request: Request) => {
   
-  // const signature = request.headers.get("stripe-signature");
+  const signature = request.headers.get("stripe-signature");
 
-  // if (!signature) {
-  //   return NextResponse.error();
-  // }
+  if (!signature) {
+    return NextResponse.error();
+  }
 
   // const text = await request.text();
 
