9320d5f3d89f1179fe6ec4e8a3d7fe21e546db1a
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-12-13 22:01:17 -0300

    fix: error webhook

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -7,47 +7,41 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 export const POST = async (request: Request) => {
-  
   const signature = request.headers.get("stripe-signature");
 
   if (!signature) {
     return NextResponse.error();
   }
 
-   const text = await request.text();
+  const text = await request.text();
 
-   try {
-    const event = stripe.webhooks.constructEvent(
-      text,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as any;
+
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
     );
+    const lineItems = sessionWithLineItems.line_items;
 
-  //   if (event.type === "checkout.session.completed") {
-  //     const session = event.data.object as any;
-
-  //     const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
-  //       event.data.object.id,
-  //       {
-  //         expand: ["line_items"],
-  //       },
-  //     );
-
-  //     const lineItems = sessionWithLineItems.line_items;
-
-  //     await prismaClient.order.update({
-  //       where: {
-  //         id: session.metadata.orderId,
-  //       },
-  //       data: {
-  //         status: "PAYMENT_CONFIRMED",
-  //       },
-  //     });
-  //   }
-
-    return NextResponse.json({ received: true });
-  } catch (error) {
-    console.error("Erro durante a verificação do webhook:", error);
-    return NextResponse.error();
+    // ATUALIZAR PEDIDO
+    await prismaClient.order.update({
+      where: {
+        id: session.metadata.orderId,
+      },
+      data: {
+        status: "PAYMENT_CONFIRMED",
+      },
+    });
   }
+
+  return NextResponse.json({ received: true });
 };
